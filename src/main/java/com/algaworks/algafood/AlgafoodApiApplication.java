587ce68d09117e6b29a44eb6e23dc085f51b3807
silvio cicoti <silvio.cicoti@gmail.com>
2020-02-01 20:08:11 -0300

    teste

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlgafoodApiApplication {
 
+	//Teste
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AlgafoodApiApplication.class, args);
 	}
